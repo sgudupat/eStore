d793331af0648bd084787db3471043eab96e5b7e
murali333 <murali.n333@gmail.com>
2015-09-03 19:03:45 +0530

    product image setting code

--- src/com/eStore/app/StoreActivity.java
+++ src/com/eStore/app/StoreActivity.java
@@ -1,38 +1,114 @@
 package com.eStore.app;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
-import com.client.vote.common.SimpleHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class StoreActivity extends Activity{
+import com.client.vote.common.LongRunningGetIO;
+import com.client.vote.common.SimpleHttpClient;
+import com.eStore.domain.Product;
+
+public class StoreActivity extends Activity implements Runnable{
+	ArrayList<Product> product = new ArrayList<Product>();
+	private Handler handler = new Handler();  
+	private ProgressDialog dialog;  
+	//String imageName="";
+	//a string array for the file name  
+	private String[] filepath; 
+	ImageView view;
+	//a variable to store the downloaded Bitmap  
+	public Bitmap downloadedBitmap;  
+	ArrayList<Product> productList;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.store);
-		   try {
-	            final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-	          
-	       
-	            String response = SimpleHttpClient.executeHttpPost("/getPrdoucts", postParameters);
-	            Log.i("Response:", response);
-	            
-	           
-	        } catch (Exception e) {
-	            Log.e("register", e.getMessage() + "");
-	            Toast.makeText(getApplicationContext(), "Products not available !!!", Toast.LENGTH_LONG).show();
-	        }
+		buildImageView();
+		  
 	}
+	private void buildImageView() {
+		// TODO Auto-generated method stub
+		/*  ListView listView = (ListView) findViewById(R.id.image_view);
+		  ProductAdapter adapter = new ProductAdapter(StoreActivity.this, generateData());
+		  listView.setAdapter(adapter);*/
+
+		productList=generateData();
+		Log.i("Imagelist size",""+productList.size());
+		//new Image(imageName,productList).execute();
+		Thread t=new Thread(this);
+		t.start();
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(int i=0;i<productList.size();i++) {
+			int resId = getResources().getIdentifier("viewImage" + i, "id", getPackageName());
+			view = (ImageView)findViewById(resId);
+			//imageName=productList.get(i).getImage();
+			view.setImageBitmap(productList.get(i).getTransformedImage());  
+			view.setVisibility(View.VISIBLE); 
+			Log.i("view id",""+view );
+			// new Image(imageName,productList).execute();
+			//view.setImageResource(R.drawable.ic_launcher);
+		}
+	}
+	/*	  private Bitmap DownloadBMP(String url) throws IOException {  
+	        //create a URL object from the passed string  
+	        URL location = new URL(url);  
+	        Get the name of the file and its path, and break it into different parts. 
+	 *Store each of these parts as elements in the filepath array.  
+	        //filepath = location.getFile().split("\u002F");  
+	        //create a InputStream object, to read data from a remote location  
+	        InputStream input_s = location.openStream();         
+	        //use the BitmapFactory to decode the downloaded stream into a Bitmap  
+	        Bitmap returnedBMP = BitmapFactory.decodeStream(input_s);  
+	        //close the InputStream  
+	        input_s.close();  
+	        //returns the downloaded bitmap  
+	        return returnedBMP;  
+	    }  
+	 */
+	//this method must be overridden, as we are implementing the Runnable interface  
+	public void run() {  
+		//Download the image  
+		try {  
+			for(int i=0;i<productList.size();i++) {
+				String imageName = productList.get(i).getImage();
+			Log.i("image name",imageName);
+			//downloadedBitmap = DownloadBMP("http://52.74.246.67:8080/images/"+imageName);  
+			URL location = new URL("http://52.74.246.67:8080/images/"+imageName);  
+			InputStream input_s = location.openStream();         
+			downloadedBitmap = BitmapFactory.decodeStream(input_s);  
+			input_s.close();
+			productList.get(i).setTransformedImage(downloadedBitmap);
+			}
+		} catch (IOException e) {  
+			//If the image couldn't be downloaded, use the standard 'image not found' bitmap  
+			downloadedBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);  
+		}  
+	}  
 	public void EstoreProfile(View view){
 		Intent intent = new Intent(this,ProfileAddressActivity.class);
 		startActivity(intent);
@@ -40,6 +116,51 @@ public class StoreActivity extends Activity{
 	public void ProductDetails (View view){
 		Intent intent = new Intent(this,ProductDetailActivity.class);
 		startActivity(intent);
+	}
+	private ArrayList<Product> generateData() {
+		String response=productDetail();
+		try {
+			JSONArray  jsonArray = new JSONArray(response);
+
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonobject = jsonArray.getJSONObject(i);
+				String category = jsonobject.getString("category");
+				String price = jsonobject.getString("price");
+				String name = jsonobject.getString("name");
+				String specification = jsonobject.getString("specification");
+				String image=jsonobject.getString("image");
+				String code=jsonobject.getString("code");
+				Log.i("product name", name);
+				Log.i("code ", code);
+				Log.i("price", price);
+				product.add(new Product(category, price, name, specification,image,code));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return product;
+
+	}
+
+
+	public String productDetail() {
+		try {
+			final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+
+
+			String response = SimpleHttpClient.executeHttpPost("/getProducts", postParameters);
+			Log.i("Response:", response);
+
+			return response;
+
+
+		} catch (Exception e) {
+			Log.e("register", e.getMessage() + "");
+			Toast.makeText(getApplicationContext(), "Products not available !!!", Toast.LENGTH_LONG).show();
+			return "fail";
+		}
+
 	}
 
 }
---
+++ src/com/eStore/domain/Product.java
@@ -0,0 +1,76 @@
+package com.eStore.domain;
+
+import android.graphics.Bitmap;
+
+public class Product {
+	
+	private String category;
+	private String price;
+	private String name;
+	private String specification;
+	private String image;
+	private Bitmap transformedImage;
+
+	public Product(String category, String price, String name,
+			String specification, String image, String code) {
+		super();
+		this.category = category;
+		this.price = price;
+		this.name = name;
+		this.specification = specification;
+		this.image = image;
+		this.code = code;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecification() {
+		return specification;
+	}
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	private String code;
+
+	public Bitmap getTransformedImage() {
+		return transformedImage;
+	}
+	public void setTransformedImage(Bitmap transformedImage) {
+		this.transformedImage = transformedImage;
+	}
+	@Override
+	public String toString() {
+		return "Product [category=" + category + ", price=" + price + ", name="
+				+ name + ", specification=" + specification + ", image="
+				+ image + ", code=" + code + "]";
+	}
+	
+
+}
