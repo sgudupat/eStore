f3a950413e2adf79b7326282587d18f2774e7f34
davidson <davidson0790@gmail.com>
2015-11-12 15:43:09 +0530

    Spinner Added into ProductDetailActivity

--- src/com/eStore/app/ProductDetailActivity.java
+++ src/com/eStore/app/ProductDetailActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class ProductDetailActivity extends Activity {
@@ -23,10 +25,19 @@ public class ProductDetailActivity extends Activity {
 		 textprice.setText(price);
 		 textinfo.setText(info);
 		 textname.setText(name);
+		 Spinner ageView = (Spinner) findViewById(R.id.product_values);
+	        // Create an ArrayAdapter using the string array and a default spinner layout
+	        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.values_array, android.R.layout.simple_spinner_item);
+	        // Specify the layout to use when the list of choices appears
+	        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	        // Apply the adapter to the spinner
+	        ageView.setAdapter(adapter);
+	        //ageView.setSelection(adapter.getPosition(age));
 		
 		 
 		ImageView view = (ImageView)findViewById(R.id.pd_productimage);
 		view.setImageBitmap(bitmap);
+		
 	}
 	public void PaymentPage(View view){
 		Intent intent = new Intent(this,PaymentActivity.class);
