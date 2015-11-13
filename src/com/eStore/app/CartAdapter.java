0bd283c3cf2a5901cbbbce4bf5cfd3fc611e4b2c
davidson <davidson0790@gmail.com>
2015-11-13 17:21:23 +0530

    CartAdapter Code pushed

---
+++ src/com/eStore/app/CartAdapter.java
@@ -0,0 +1,46 @@
+package com.eStore.app;
+
+import java.util.ArrayList;
+
+import android.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+import com.client.vote.domain.Cart;
+
+public class CartAdapter extends BaseAdapter implements ListAdapter {
+private ArrayList<Cart> cart=new ArrayList<Cart>();
+private Context context;
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout., null);
+		}
+		return null;
+	}
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
